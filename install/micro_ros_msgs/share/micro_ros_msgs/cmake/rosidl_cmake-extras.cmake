962da58f8ceddd84314837b70f1e9007f0061bed
Robot :) <axellundin@gmail.com>
2025-02-03 13:04:35 +0100

    Fixed things

--- install/micro_ros_msgs/share/micro_ros_msgs/cmake/rosidl_cmake-extras.cmake
+++ install/micro_ros_msgs/share/micro_ros_msgs/cmake/rosidl_cmake-extras.cmake
@@ -1,4 +1,1 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(micro_ros_msgs_IDL_FILES "msg/Graph.idl;msg/Node.idl;msg/Entity.idl")
-set(micro_ros_msgs_INTERFACE_FILES "msg/Graph.msg;msg/Node.msg;msg/Entity.msg")
+/home/sleepy/robp-group7-sleepy/build/micro_ros_msgs/rosidl_cmake/rosidl_cmake-extras.cmake
